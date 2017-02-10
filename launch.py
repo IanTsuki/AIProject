7c77dc8f2de685590f1dbe8da3b8a9e6ab152904
IanTsuki <dylanjvanderwal@gmail.com>
2017-02-10 20:04:49 +1000

    base of languge recognition

--- launch.py
+++ launch.py
@@ -1,1 +1,15 @@
-print ("Welcome Master, Is this your first time running me?")
+print("Hello,")
+print("I am your personal assistant IDA.")
+print("At the moment I am in Alpha 1, so please excuse any problems with my systems.")
+
+
+meow = "death"
+while meow != "close":
+    meow = input()
+    meow = meow.lower()
+    if "how" in meow and "are" in meow and "you" in meow:
+        print("I am good.")
+    if "hello" in meow:
+        print("Hello, what can I do for you today?")
+    if "who" in meow and "are" in meow and "you" in meow:
+        print("I am IDA, the Intiligent Desktop Application.")
